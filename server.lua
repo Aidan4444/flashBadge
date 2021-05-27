17287816b338356c628acc96d113c00e07279337
Aidan_ogg <49934724+Aidan4444@users.noreply.github.com>
2021-05-27 00:30:04 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -7,15 +7,21 @@ AddEventHandler('flashBadge:server:checkAcePermission', function()
     end
 end)
 
+function GetVersion()
+    local verFile = LoadResourceFile(GetCurrentResourceName(), "version.json")
+    local verContent = json.decode(verFile)
+    return verContent.fivem.version
+end
+
 curVersion = GetVersion()
-local updatePath = "/Aidan4444/flashBadge"
+local updatePath = "Aidan4444/flashBadge"
 local resourceName = "Flash Badge ("..GetCurrentResourceName()..")"
 function checkVersion(err,response, headers)
     if err == 200 then
         local data = json.decode(response)
         local remoteVersion = data.fivem.version
         local changelog = data.fivem.changelog
-        PrintDebugMessage("Version check returned "..err..", Local Version: "..curVersion..", Remote Version: "..remoteVersion, 4)
+        print("^5" .. GetCurrentResourceName() .. "^7: Version check returned "..err..", Local Version: "..curVersion..", Remote Version: "..remoteVersion .. "^7\n")
         if curVersion ~= remoteVersion and tonumber(curVersion) < tonumber(remoteVersion) then
             print("\n--------------------------------------------------------------------------")
             print("\n"..resourceName.." is outdated.\nNewest Version: "..remoteVersion.."\nYour Version: "..curVersion.."\nPlease update it from https://github.com"..updatePath.."")
@@ -28,7 +34,7 @@ function checkVersion(err,response, headers)
             --print(resourceName.." is up to date!")
         end
     else
-        PrintDebugMessage(resourceName .. " Version Check failed, please make sure its updated!")
+        print(resourceName .. " Version Check failed, please make sure its updated!")
     end
     
     SetTimeout(3600000, checkVersionHTTPRequest)
@@ -36,11 +42,6 @@ end
 
 function checkVersionHTTPRequest()
     PerformHttpRequest("https://raw.githubusercontent.com/"..updatePath.."/master/version.json", checkVersion, "GET")
-end
-
-function GetVersion()
-    local verFile = LoadResourceFile(GetCurrentResourceName(), "version.json")
-    local verContent = json.decode(verFile)
 end
 
 checkVersionHTTPRequest()
