3a1d5a4a362ff1e84fcd8530ec3a67f19cbfa0df
Aidan_ogg <49934724+Aidan4444@users.noreply.github.com>
2021-05-27 00:29:39 -0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,38 +1,38 @@
 RegisterNetEvent('flashBadge:client:animation')
 AddEventHandler('flashBadge:client:animation', function()
     Citizen.CreateThread(function()
-        local ped = GetPlayerPed(-1)
+        local ped = PlayerPedId()
         local coords = GetEntityCoords(ped)
         local badgeProp = CreateObject(GetHashKey(config.propName), coords.x, coords.y, coords.z + 0.2, true, true, true)
         local boneIndex = GetPedBoneIndex(ped, 28422)
         
-        AttachEntityToEntity(prop, ped, boneIndex, 0.065, 0.029, -0.035, 80.0, -1.90, 75.0, true, true false, true, 1, true)
+        AttachEntityToEntity(badgeProp, ped, boneIndex, 0.065, 0.029, -0.035, 80.0, -1.90, 75.0, true, true, false, true, 1, true)
         RequestAnimDict('paper_1_rcm_alt1-9')
-        TaskPlayAnim(ped, 'paper_1_rcm_alt_1-9', 'player_one_dual-9', 8.0, -8, 10.0, 49, 0, 0, 0, 0)
+        TaskPlayAnim(ped, 'paper_1_rcm_alt1-9', 'player_one_dual-9', 8.0, -8, 10.0, 49, 0, 0, 0, 0)
         Citizen.Wait(3000)
         ClearPedSecondaryTask(ped)
         DeleteObject(badgeProp)
     end)
 end)
 
-if config.commandSuggestion and config.useCommand then 
-    TriggerEvent('chat:addSuggestion', config.commandName, 'Flashes your badge.')
-end
+Citizen.CreateThread(function()
+    if config.commandSuggestion and config.useCommand then 
+        TriggerEvent('chat:addSuggestion', '/' .. config.commandName, 'Flashes your badge.')
+    end
+end)
 
-RegisterCommand(config.commandName, function(source, args, rawCommand)
-    if source > 0 then 
-        if config.useCommand then 
-            if leoAcePerm then
-                TriggerEvent('flashBadge:client:animation')
-            else
-                TriggerEvent('chat:addMessage', {
-                    color = {255, 92, 92},
-                    multiline = true,
-                    args = {'[Error]', cofig.commandErrorMessage}
-                })
-            end
+RegisterCommand(config.commandName, function()
+    if config.useCommand then 
+        if leoAcePerm then
+            TriggerEvent('flashBadge:client:animation')
+        else
+            TriggerEvent('chat:addMessage', {
+                color = {255, 92, 92},
+                multiline = true,
+                args = {'[Error]', config.commandErrorMessage}
+            })
         end
-    end 
+    end
 end, false)
 
 RegisterKeyMapping('+flashBadge', 'Flash Badge', 'keyboard', config.keybind)
